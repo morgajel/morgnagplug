8fce0142ca079f437ae0eaa47a80df916881bc82
Jesse Morgan <morgajel@gmail.com>
2014-04-26 00:21:33 -0400

    apache status now functional

--- check_apachestatus.py
+++ check_apachestatus.py
@@ -25,10 +25,12 @@
 # 20130326 <morgajel at gmail dot com> v1.6
 #          rewriting in python
 #
-import pynagios
 import urllib2
+import pynagios
 from pynagios import Plugin, Response, make_option
 import re
+import sys
+import time
 class MyCheck(Plugin):
     ssl  = make_option("--ssl",  action="store_true")
     port = make_option("-p", "--port",      type="int", default="80")
@@ -38,8 +40,13 @@ class MyCheck(Plugin):
         #because plugin timeout of zero and urllib2's timeout of zero do different things...
         if self.options.timeout == 0:
             self.options.timeout=None
+        if self.options.warning == None:
+            self.options.warning='10'
+        if self.options.critical == None:
+            self.options.critical='20'
 
         try:
+            start = time.time()
             if self.options.ssl: 
                 website = urllib2.urlopen('https://%s:%i/server-status?auto' % 
                             ( self.options.hostname, self.options.port),None, self.options.timeout )
@@ -48,7 +55,10 @@ class MyCheck(Plugin):
                             ( self.options.hostname, self.options.port), None, self.options.timeout )
             
             content = website.read().strip()
+            end = time.time()
             results = dict(re.split(':\s*', line) for line in content.split('\n'))
+            results['ResponseTime']=end-start
+            #results['scores']= dict((char,results['Scoreboard'].count(char)) for char in  "_SRWKDCLGI."    )
 
         except urllib2.HTTPError, e:
             return Response(pynagios.UNKNOWN, 
@@ -60,51 +70,32 @@ class MyCheck(Plugin):
         return self.response_for_value(results)
 
     def response_for_value(self, results):
-        response=Response(pynagios.OK);
-        response.set_perf_data('total accesses', results['Total Accesses'], uom='c', )
-        response.set_perf_data('total kBytes', results['Total kBytes'], uom='kb', )
+        if results['ResponseTime'] >  self.options.critical:
+            response=Response(pynagios.CRITICAL, "Response time greater than critical")
+        elif results['ResponseTime'] >  self.options.warning:
+            response=Response(pynagios.WARNING, "Response time greater than warning")
+        else:
+            response=Response(pynagios.OK, "Response time is ok")
+        
+
+        response.set_perf_data('Total Accesses', results['Total Accesses'], uom='c', )
+        response.set_perf_data('Total kBytes', results['Total kBytes'], uom='kb', )
         response.set_perf_data('CPULoad', float(results['CPULoad'])*100, uom='%', )
         response.set_perf_data('Uptime', results['Uptime'], uom='c', )
+        response.set_perf_data('ReqPerSec', results['ReqPerSec'], )
+        response.set_perf_data('BytesPerSec', results['BytesPerSec'], uom='b', )
+        response.set_perf_data('BytesPerReq', results['BytesPerReq'], uom='b', )
+        response.set_perf_data('BusyWorkers', results['BusyWorkers'],  )
+        response.set_perf_data('IdleWorkers', results['IdleWorkers'],  )
+        response.set_perf_data('ResponseTime', results['ResponseTime'], uom='s',warn=self.options.warning, crit=self.options.critical )
 
         return response
 
-#ReqPerSec: .0207126
-#BytesPerSec: 313.067
-#BytesPerReq: 15114.8
 #BusyWorkers: 1
 #IdleWorkers: 13
 #Scoreboard: ____________W_..................................................................................................................................................................................................................................................................................................................................................................................................
 #
 #
-
-
-#Note :
-#  The script will return
-#    * Without warn and critical options:
-#        OK       if we are able to connect to the apache server's status page,
-#        CRITICAL if we aren't able to connect to the apache server's status page,,
-#    * With warn and critical options:
-#        OK       if we are able to connect to the apache server's status page and #available slots > <warn_level>,
-#        WARNING  if we are able to connect to the apache server's status page and #available slots <= <warn_level>,
-#        CRITICAL if we are able to connect to the apache server's status page and #available slots <= <crit_level>,
-#        UNKNOWN  if we aren't able to connect to the apache server's status page
-#
-#Perfdata legend:
-#"_;S;R;W;K;D;C;L;G;I;.;1;2;3"
-#_ : Waiting for Connection
-#S : Starting up
-#R : Reading Request
-#W : Sending Reply
-#K : Keepalive (read)
-#D : DNS Lookup
-#C : Closing connection
-#L : Logging
-#G : Gracefully finishing
-#I : Idle cleanup of worker
-#. : Open slot with no current process
-#1 : Requests per sec
-#2 : kB per sec
-#3 : kB per Request
 
 if __name__ == "__main__":
     # Instantiate the plugin, check it, and then exit
