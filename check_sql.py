2b97ebee8e71c090bca180204f54580afffba445
Jesse Morgan <morgajel@gmail.com>
2014-04-29 11:20:20 -0400

    added mssql support, switched to pynag

--- check_sql.py
+++ check_sql.py
@@ -1,114 +1,70 @@
 #!/usr/bin/env python
-# check_sql - a monitoring plugin for databases
-# Copyright (C) 2014 Jesse Morgan
-# BASED ON: check_oracle_generic, Copyright 2006 David Ligeret (david.ligeret at gmail.com)
-#
-# check_sql
-# This file is part of morgnagplug
-#
-# check_sql is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# check_sql is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+####################### check_apachestatus.py #######################
+# Licence : GPL - http://www.fsf.org/licenses/gpl.txt
+#############################################################
+# Inspired by check_oracle_generic, Copyright 2006 David Ligeret (david.ligeret at gmail.com)
 
-prog = "check_sql"
-prog_version = "3.0"
-
+from pynag.Plugins import PluginHelper,ok,warning,critical,unknown
+import re
+import time
 import os
 import sys
-import getopt
-import nagiosplugin
-import argparse
-import logging
 from sqlalchemy import create_engine
 from sqlalchemy import databases
 from sqlalchemy.sql import text
 
-_log = logging.getLogger('nagiosplugin')
+
+my_plugin = PluginHelper()
+
+my_plugin.parser.add_option('-w', '--warning',  default="0:5", help='return warning if sql is outside RANGE')
+my_plugin.parser.add_option('-c', '--critical', default="0:10", help='return critical if sql is outside RANGE')
+my_plugin.parser.add_option('-H', '--hostname',   default="localhost"   )
+my_plugin.parser.add_option('-P', '--password',   default='')
+my_plugin.parser.add_option('-U', '--user',       default='')
+my_plugin.parser.add_option('-D', '--driver',     default='mysql')
+my_plugin.parser.add_option('-s', '--sid',        default='', help='sid/DB name for oracle driver')
+my_plugin.parser.add_option(      '--dsn',        default='', help='DSN (required for ODBC driver)')
+my_plugin.parser.add_option(      '--dbname',     default='', help='DB name to query')
+my_plugin.parser.add_option('-f', '--file',       default='', help='file containing SQL (should return a number)')
+my_plugin.parser.add_option('-u', '--units',      default='', help='Units of Returned Value')
+my_plugin.parser.add_option('-p', '--port',       default='3306')
+my_plugin.parser.add_option('-q', '--query',      default='select 1')
+
+my_plugin.parse_arguments()
+
+if my_plugin.options.driver == 'oracle' and my_plugin.options.sid == '':
+    my_plugin.exit(summary="Oracle driver requires a --sid" , exit_code=unknown)
+elif my_plugin.options.driver == 'odbc' and my_plugin.options.dsn == '':
+    my_plugin.exit(summary="ODBC requires a --dsn" , exit_code=unknown)
+try:
+    if my_plugin.options.driver == 'mssql':
+        engine = create_engine("mssql+pymssql://%s:%s@%s/%s" % (my_plugin.options.user, my_plugin.options.password, my_plugin.options.hostname, my_plugin.options.dbname) ) 
+        results=engine.execute( text(my_plugin.options.query) ).fetchall()[0][0]
+    elif my_plugin.options.driver == 'mysql':
+        engine = create_engine("mysql+pymysql://%s:%s@%s/%s" % (my_plugin.options.user, my_plugin.options.password, my_plugin.options.hostname, my_plugin.options.dbname) ) 
+        results=engine.execute( text(my_plugin.options.query) ).fetchall()[0][0]
+    else:
+        my_plugin.exit(summary="This driver is not currently supported, sorry." , exit_code=unknown)
+except ImportError, e:
+    my_plugin.exit(summary="Something is missing: %s" % str(e)   , long_output="You may be missing critical python modules.", exit_code=unknown, perfdata='')
+except Exception, e:
+    my_plugin.exit(summary="Something horrible happened: %s " % e.__class__ , long_output=str(e), exit_code=unknown, perfdata='')
+
+# Lets Parse the data:
+my_plugin.add_summary( "%s result" % results)
+
+# and add metrics:
+my_plugin.add_metric( label='result', value=results, warn=my_plugin.options.warning, crit=my_plugin.options.critical )
+
+# By default assume everything is ok. Any thresholds specified with --threshold can overwrite this status:
+my_plugin.status(ok)
+
+# Here all metrics will be checked against thresholds that are either
+# built-in or added via --threshold from the command-line
+my_plugin.check_all_metrics()
+
+# Print out plugin information and exit nagios-style
+my_plugin.exit()
 
 
-class SQL(nagiosplugin.Resource):
 
-    def __init__(self,args):
-        self.args=args
-        
-
-    def probe(self):
-        _log.info('attempting to connect to the DB')
-        # faking a result of 10 returned rows
-        args=self.args
-        if args.driver == 'mysql':
-            engine = create_engine("mysql+pymysql://test:test@localhost/test") 
-            results=engine.execute( text(args.query) ).fetchall()[0][0]
-            yield nagiosplugin.Metric('rows', results, min=0, context='sql')
-        else:
-            raise nagiosplugin.CheckError("This driver is not currently supported, sorry.") 
-
-
-class SQLContext(nagiosplugin.ScalarContext):
-
-    def __init__(self, name, args):
-        nagiosplugin.ScalarContext.__init__(self, name, args.warning, args.critical)
-        self.args=args
-        if args.driver == 'oracle' and args.sid == '':
-            raise nagiosplugin.CheckError("Oracle driver requires a --sid") 
-        elif args.driver == 'odbc' and args.dsn == '':
-            raise nagiosplugin.CheckError("ODBC requires a --dsn") 
-    def evaluate(self, metric, resource):
-        result= nagiosplugin.ScalarContext.evaluate(self, metric, resource)
-        
-        return result
-
-
-class SQLSummary(nagiosplugin.Summary):
-    """Resulting Query Information."""
-
-    def ok(self, results):
-        return 'SQL results for %s' % str(results['rows'])
-
-@nagiosplugin.guarded
-def main():
-    argp = argparse.ArgumentParser(description=__doc__)
-    argp.add_argument('-w', '--warning', metavar='RANGE', default="1:100",
-                      help='return warning if sql is outside RANGE')
-    argp.add_argument('-c', '--critical', metavar='RANGE', default="1:100",
-                      help='return critical if sql is outside RANGE')
-    argp.add_argument('-H', '--host',       default='localhost')
-    argp.add_argument('-P', '--password',   default='')
-    argp.add_argument('-U', '--user',       default='')
-    argp.add_argument('-D', '--driver',     default='mysql')
-    argp.add_argument('-s', '--sid',        default='', help='sid/DB name for oracle driver')
-    argp.add_argument(      '--dsn',        default='', help='DSN (required for ODBC driver)')
-    argp.add_argument(      '--dbname',     default='', help='DB name to query')
-    argp.add_argument('-f', '--file',       default='', help='file containing SQL (should return a number)')
-    argp.add_argument('-u', '--units',      default='', help='Units of Returned Value')
-    argp.add_argument('-p', '--port',       default='3306')
-    argp.add_argument('-q', '--query',      default='select 1')
-    argp.add_argument('-v', '--verbose', action='count', default=0,
-                      help='increase output verbosity (use up to 3 times)')
-    argp.add_argument('-t', '--type',       default='', help="""type of threshold (return value is over|under|range|equal WARN or CRIT; default is "under")
-        if VALUE is over 3, WARN;
-        if VALUE is under 3, WARN;
-        if VALUE is equal 3, WARN;
-        if VALUE is unequal 3, WARN;
-        if VALUE is inside (15,20), WARN;
-        if VALUE is outside (4,30), WARN;""")
-    args = argp.parse_args()
-
-
-    check = nagiosplugin.Check(
-        SQL(args),
-        SQLContext('sql', args),
-        SQLSummary() )
-    check.main(verbose=args.verbose)
-
-if __name__ == '__main__':
-    main()
